f0175c7e93f53a454aa92a3b1b2ac514d62520a0
krapaomukrob <96363202+Pannathornnn@users.noreply.github.com>
2026-01-13 23:52:48 +0700

    Update lab14_2.cpp

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,42 @@ int main(){
 }
 
 //Do not modify source code above this line 
+
+void inputMatrix(double A[N][N])
+{
+	for(int current_row = 0 ; current_row < N ; current_row++)
+	{
+		cout << "Row " << current_row+1 << ": " ;
+		for(int current_col = 0 ; current_col < N ; current_col++)
+		{
+			cin >> A[current_row][current_col] ;
+		}
+	}
+}
+
+void showMatrix(const bool B[N][N])
+{
+	for(int current_row = 0 ; current_row < N ; current_row++)
+	{
+		for(int current_col = 0 ; current_col < N ; current_col++)
+		{
+			cout << B[current_row][current_col] << " ";
+		}
+		cout << endl ;
+	}
+}
+
+void findLocalMax(const double A[N][N], bool B[N][N])
+{
+	for(int current_row = 1 ; current_row < N-1 ; current_row++)
+	{
+		for(int current_col = 1 ; current_col < N-1 ; current_col++)
+		{
+			if(A[current_row][current_col] >= A[current_row-1][current_col] && A[current_row][current_col] >= A[current_row+1][current_col] && A[current_row][current_col] >= A[current_row][current_col-1] && A[current_row][current_col] >= A[current_row][current_col+1])
+			{
+				B[current_row][current_col] = true ;
+			}
+			else B[current_row][current_col] = false ;
+		}
+	}
+}
